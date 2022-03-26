1d60f1f86f5825f2f2614f1e4d1a90c372b7893b
Kirill Balakhnev <wolfram3387@yandex.ru>
2022-03-26 16:51:49 +0300

    26.03

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# from handlers.users.functions import watch_notifications
 from utils.set_bot_commands import set_default_commands
 
 
@@ -22,8 +21,8 @@ async def on_shutdown(dp):
 if __name__ == '__main__':
     from aiogram import executor
     from handlers import dp
+    # from handlers.users.functions import watch_notifications
 
     # thread = threads.to_thread(watch_notifications, kwargs=dict(dp=dp))    # 1 поток (дополнительный)
     # thread.start()
     executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)    # 2 поток (основной)
-# 123
